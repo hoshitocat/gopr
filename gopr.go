ec7a87ce960aa3a66932b95d4bf76a30c27dbfa0
dorocy <hoshitocat@gmail.com>
2019-09-04 14:17:19 +0900

    :tada: Get repositories from github api with google go client

--- gopr.go
+++ gopr.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"flag"
+	"fmt"
+
+	"github.com/google/go-github/v28/github"
+	"golang.org/x/oauth2"
+)
+
+var (
+	token = flag.String("token", "", "Your github access token")
+)
 
 func main() {
-	fmt.Println("gopr")
+	flag.Parse()
+
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: *token})
+	client := github.NewClient(oauth2.NewClient(ctx, ts))
+	repos, _, err := client.Repositories.List(ctx, "", nil)
+	if err != nil {
+		// TODO: Error handling
+		panic(err)
+	}
+
+	fmt.Println(repos)
 }
