81a7e202cf5610640683be813aecaf70fd9989cc
dorocy <hoshitocat@gmail.com>
2019-09-05 23:51:49 +0900

    :bug: Fix origin name parser

--- gopr.go
+++ gopr.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"os/exec"
+	"regexp"
 
 	"github.com/google/go-github/v28/github"
 	"golang.org/x/oauth2"
@@ -21,18 +22,26 @@ func main() {
 	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: *token})
 	client := github.NewClient(oauth2.NewClient(ctx, ts))
 
-	_, _, err := client.Repositories.List(ctx, "", nil)
-	if err != nil {
-		// TODO: Error handling
-		panic(err)
-	}
-
 	originURL, err := exec.CommandContext(ctx, "git", "config", "--get", "remote.origin.url").Output()
 	if err != nil {
 		// TODO: Error handling
 		panic(err)
 	}
-	repoName := string(originURL)
 
-	fmt.Println(repoName)
+	exp := regexp.MustCompile(`git@github\.com:(?P<owner>.+)/(?P<repo>.+)`)
+	match := exp.FindSubmatch(originURL)
+	originNames := make(map[string]string)
+	for i, name := range exp.SubexpNames() {
+		if i != 0 && name != "" {
+			originNames[name] = string(match[i])
+		}
+	}
+	ownerName, repoName := originNames["owner"], originNames["repo"]
+	repo, _, err := client.Repositories.Get(ctx, ownerName, repoName)
+	if err != nil {
+		// TODO: Error handling
+		panic(err)
+	}
+
+	fmt.Println(repo)
 }
